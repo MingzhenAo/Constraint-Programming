fc32cbf708f6f93221e477e27b7930a46a825d5d
Mingzhen Ao <u6527600@anu.edu.au>
2020-10-03 04:15:23 +1000

    update

--- IQtwist/izplus.all.py
+++ IQtwist/izplus.all.py
@@ -7,12 +7,13 @@ Created on Wed Sep 30 22:30:40 2020
 from subprocess import STDOUT, check_output
 import time
 import os
+import win32api
+import win32con
 #script
-difficulties=['start','junior','master','expert','wizard']
+difficulties=['wizard']
 for dif in difficulties:
-  i=12
-  while i<25:
-    command='docker exec mycontainer /bin/bash -c "solver  /test_container/Desktop/project-2020-s2-puzzle-constraints/IQtwist/'+dif+'/model'+str(i)+'/model'+str(i)+'.mzn"'
+  for i in range(16,25):
+    command='docker exec mycontainer /bin/bash -c "timeout 1805 solver  /test_container/Desktop/project-2020-s2-puzzle-constraints/IQtwist/'+dif+'/model'+str(i)+'/model'+str(i)+'.mzn"'
     file = open(dif+'/model'+str(i)+'/'+'model' + str(i) +"izplus"+'.log', 'w')
     try:
        begin=time.time()
@@ -21,7 +22,5 @@ for dif in difficulties:
        file.write(text_string)
        end = time.time()
        file.write("the excute time:"+str(end-begin))
-    except Exception as e:
-        print(e)
+    except Exception:
         file.write('timeout-30minutes\n')
-    i+=1
---
+++ ZIGZAGpuzzler/izplus3Dall.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  2 20:39:48 2020
+
+@author: Mingzhen Ao
+"""
+from subprocess import STDOUT, check_output
+import time
+import os
+import win32api
+import win32con
+modes=['playingmode2']
+difficulties=['start','junior','expert','master','wizard']
+for mode in modes:
+   for dif in difficulties:
+     for i in range(1,9):
+        command='docker exec mycontainer /bin/bash -c "timeout 1805 solver  /test_container/Desktop/project-2020-s2-puzzle-constraints/ZIGZAGpuzzler/'+mode+'/'+dif+'/model'+str(i)+'/model'+str(i)+'.mzn"'
+        file = open(mode+'/'+dif+'/model'+str(i)+'/'+'model' + str(i) +"izplus"+'.log', 'w')
+        try:
+           begin=time.time()
+           text= check_output(command, stderr=STDOUT, timeout=1800,shell=True)
+           text_string = text.decode(encoding='UTF-8')
+           file.write(text_string)
+           end = time.time()
+           file.write("the excute time:"+str(end-begin))
+        except Exception:
+            file.write('timeout-30minutes\n')
